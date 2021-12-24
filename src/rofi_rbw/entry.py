eb069f17bbf711bfc664f3a0ee0b59a717ccc64e
Fabian Winter <5821180+fdw@users.noreply.github.com>
2021-12-24 10:21:29 +0100

    Parse formatted string better
    Why clutter the main class with parsing if `Entry` is the guilty party?
    Also, this might be the first time that a regex makes for a cleaner
    solution 🤯
    
    Co-Authored-By: Fabian Winter <5821180+fdw@users.noreply.github.com>
    Co-Authored-By: tosti007 <brian-janssen@hotmail.com>

--- src/rofi_rbw/entry.py
+++ src/rofi_rbw/entry.py
@@ -1,25 +1,36 @@
-from typing import Union, List
+import re
 
 
 class Entry:
-
-    def __init__(self, data: str) -> None:
-        self.name = ''
-        self.folder = ''
-        self.username = ''
-
-        fields = data.split('\t')
-        self.name = fields[1]
-        self.folder = fields[0]
+    def __init__(self, name: str = '', folder: str = '', username: str = '') -> None:
+        self.name = name
+        self.folder = folder
+        self.username = username
         self.length = len(self.name) + len(self.folder) + 1
-
-        try:
-            self.username = fields[2]
-        except IndexError:
-            self.username = ''
 
     def __len__(self) -> int:
         return self.length
 
     def formatted_string(self, max_width: int) -> str:
         return f'{self.folder}/<b>{self.name.ljust(max_width - len(self.folder))}</b>{self.username}'
+
+    @staticmethod
+    def parse_formatted_string(formatted_string: str) -> 'Entry':
+        regex = re.compile('(?P<folder>.*)/<b>(?P<name>.*)</b>(?P<username>.*)')
+        match = regex.search(formatted_string)
+
+        return Entry(
+            match.group('name').strip(),
+            match.group('folder'),
+            match.group('username').strip()
+        )
+
+    @staticmethod
+    def parse_rbw_output(rbw_string: str) -> 'Entry':
+        fields = rbw_string.split('\t')
+
+        return Entry(
+            fields[1],
+            fields[0],
+            fields[2] if len(fields) > 2 else ''
+        )
