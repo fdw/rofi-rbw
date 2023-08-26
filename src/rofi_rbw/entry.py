f1cb1af1a7a9ad57e8f3c4b8429ffec2c063b9d5
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-08-26 12:44:15 +0200

    entry: add some doctests

--- src/rofi_rbw/entry.py
+++ src/rofi_rbw/entry.py
@@ -1,9 +1,9 @@
-from re import compile
 from dataclasses import dataclass
+from re import compile
 from typing import Optional
 
-RE = compile("(?:(?P<folder>.+?)/)?(?P<name>.*?)? *  (?P<username>.*)?")
-RE_MARKUP = compile("(?:(?P<folder>.+?)/)?<b>(?P<name>.*)</b> *  (?P<username>.*)?")
+RE = compile("(?:(?P<folder>.+)/)?<b>(?P<name>.*)</b> *  (?P<username>.+)?")
+RE_NO_M = compile("(?:(?P<folder>.+)/)?(?P<name>.*?)? *  (?P<username>.+)?")
 
 
 @dataclass(frozen=True)
@@ -12,7 +12,23 @@ class Entry:
     folder: Optional[str] = ""
     username: Optional[str] = ""
 
-    def format(self, max_width: int, show_folders: bool, use_markup: bool):
+    def format(self, max_width: int, show_folders: bool, use_markup: bool) -> str:
+        """Format this Entry as str.
+
+        >>> entry = Entry(name="test", folder="path/sub", username="joe")
+        >>> entry.format(40, True, True)
+        'path/sub/<b>test</b>                      joe'
+        >>> entry.format(40, False, True)
+        '<b>test</b>                               joe'
+        >>> entry.format(40, True, False)
+        'path/sub/test                             joe'
+        >>> entry.format(0, True, True)
+        'path/sub/<b>test</b>  joe'
+        >>> entry = Entry(name="name_only")
+        >>> entry.format(40, True, True)
+        '<b>name_only</b>                          '
+        """
+
         folder = f"{self.folder}/" if show_folders and self.folder else ""
         name = f"<b>{self.name}</b>" if use_markup else self.name
         start = f"{folder}{name}"
@@ -20,5 +36,23 @@ class Entry:
 
     @staticmethod
     def parse(formatted_string: str, use_markup: bool) -> "Entry":
-        match = (RE_MARKUP if use_markup else RE).search(formatted_string)
+        """Parse str as Entry.
+
+        >>> Entry.parse('path/sub/<b>test</b>  joe', True)
+        Entry(name='test', folder='path/sub', username='joe')
+        >>> Entry.parse('path/sub/<b>test</b>                      joe', True)
+        Entry(name='test', folder='path/sub', username='joe')
+        >>> Entry.parse('path/sub/test                             joe', False)
+        Entry(name='test', folder='path/sub', username='joe')
+        >>> Entry.parse('<b>name_only</b>                          ', True)
+        Entry(name='name_only', folder=None, username=None)
+        """
+
+        match = (RE if use_markup else RE_NO_M).search(formatted_string)
         return Entry(**match.groupdict())
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
