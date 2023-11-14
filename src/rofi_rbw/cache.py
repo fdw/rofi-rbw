73947ed8a1b85df324ff4352f41fa8f824bf0824
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-14 12:43:36 +0100

    move cache logic into rofi_rbw

--- src/rofi_rbw/cache.py
+++ src/rofi_rbw/cache.py
@@ -9,19 +9,27 @@ class Cache:
         cache_file.parent.mkdir(exist_ok=True)
         cache_file.touch(exist_ok=True)
 
-        self.cache = {entry.sha1: (0, entry) for entry in entries}
+        self.cache = {entry.sha1: [0, entry] for entry in entries}
 
         with cache_file.open() as f:
             for line in f:
                 if line.strip():
-                    n, hashed_entry = line.strip().split(" ", maxsplit=1)
-                    self.cache[hashed_entry][0] = int(n)
+                    n, sha1 = line.strip().split(" ", maxsplit=1)
+                    if sha1 in self.cache:
+                        self.cache[sha1][0] = int(n)
 
-    def sorted(self, entries: List[Entry]) -> List[Entry]:
-        return [e for _, e in sorted(self.cache.values())]
+    def sorted(self) -> List[Entry]:
+        return [
+            e
+            for _, e in sorted(
+                self.cache.values(),
+                key=lambda t: t[0],
+                reverse=True,
+            )
+        ]
 
     def update(self, entry: Entry):
         self.cache[entry.sha1][0] += 1
         with cache_file.open("w") as f:
-            for n, e in sorted(self.cache.values()):
-                f.write(f"{n} {e}")
+            for n, e in self.cache.values():
+                f.write(f"{n} {e.sha1}\n")
