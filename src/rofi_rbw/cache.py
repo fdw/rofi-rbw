1c2a39f9e0f10d38c7b57d1833b1391534989364
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-14 12:03:44 +0100

    rework cache

--- src/rofi_rbw/cache.py
+++ src/rofi_rbw/cache.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from typing import List
 
 from .entry import Entry
@@ -6,31 +5,23 @@ from .paths import cache_file
 
 
 class Cache:
-    def __init__(self):
+    def __init__(self, entries: List[Entry]):
         cache_file.parent.mkdir(exist_ok=True)
         cache_file.touch(exist_ok=True)
 
-        self.cache = []
+        self.cache = {entry.sha1: (0, entry) for entry in entries}
 
         with cache_file.open() as f:
             for line in f:
                 if line.strip():
                     n, hashed_entry = line.strip().split(" ", maxsplit=1)
-                    self.cache.append((int(n), hashed_entry))
+                    self.cache[hashed_entry][0] = int(n)
 
-    def sort(self, entries: List[Entry]) -> List[Entry]:
-        hashed_entries = {entry.sha1: entry for entry in entries}
-        for _, hashed_entry in self.cache:
-            if hashed_entry in hashed_entries.keys():
-                entries.remove(hashed_entries[hashed_entry])
-                entries = [hashed_entries[hashed_entry], *entries]
-
-        return entries
+    def sorted(self, entries: List[Entry]) -> List[Entry]:
+        return [e for _, e in sorted(self.cache.values())]
 
     def update(self, entry: Entry):
-        hashed_entries = defaultdict(int, {h: n for n, h in self.cache})
-        hashed_entries[entry.sha1] += 1
-        lines = [(n, h) for h, n in hashed_entries.items()]
+        self.cache[entry.sha1][0] += 1
         with cache_file.open("w") as f:
-            for n, e in sorted(lines):
+            for n, e in sorted(self.cache.values()):
                 f.write(f"{n} {e}")
