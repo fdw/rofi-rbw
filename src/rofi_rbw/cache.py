a053e38e8433ec065eff126190f5f9aa80b187f0
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-14 13:45:25 +0100

    cache: don't touch it

--- src/rofi_rbw/cache.py
+++ src/rofi_rbw/cache.py
@@ -7,16 +7,17 @@ from .paths import cache_file
 class Cache:
     def __init__(self, entries: List[Entry]):
         cache_file.parent.mkdir(exist_ok=True)
-        cache_file.touch(exist_ok=True)
 
         self.cache = {entry.sha1: [0, entry] for entry in entries}
 
-        with cache_file.open() as f:
-            for line in f:
-                if line.strip():
-                    n, sha1 = line.strip().split(" ", maxsplit=1)
-                    if sha1 in self.cache:
-                        self.cache[sha1][0] = int(n)
+        if cache_file.exists():
+            with cache_file.open() as f:
+                for line in f:
+                    sline = line.strip()
+                    if sline:
+                        n, sha1 = sline.split(" ", maxsplit=1)
+                        if sha1 in self.cache:
+                            self.cache[sha1][0] = int(n)
 
     def sorted(self) -> List[Entry]:
         return [
