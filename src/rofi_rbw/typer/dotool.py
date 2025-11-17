785d0281d51f3af1ac2878c0b97f18abd2080fec
Joris29 <joris.vanacoleyen@bryxx.eu>
2025-11-17 13:52:54 +0100

    Add a type start delay option

--- src/rofi_rbw/typer/dotool.py
+++ src/rofi_rbw/typer/dotool.py
@@ -1,4 +1,5 @@
 from subprocess import run
+from time import sleep
 
 from ..abstractionhelper import is_installed
 from .typer import Key, Typer
@@ -16,7 +17,8 @@ class DotoolTyper(Typer):
     def get_active_window(self) -> str:
         return "not possible with dotool"
 
-    def type_characters(self, characters: str, key_delay: int, active_window: str) -> None:
+    def type_characters(self, characters: str, start_delay: float, key_delay: int, active_window: str) -> None:
+        sleep(start_delay)
         run(["dotool"], input=f"typedelay {key_delay}\ntype {characters}", encoding="utf-8")
 
     def press_key(self, key: Key) -> None:
--- src/rofi_rbw/typer/wtype.py
+++ src/rofi_rbw/typer/wtype.py
@@ -1,4 +1,5 @@
 from subprocess import run
+from time import sleep
 
 from ..abstractionhelper import is_installed, is_wayland
 from .typer import Key, Typer
@@ -16,7 +17,8 @@ class WTypeTyper(Typer):
     def get_active_window(self) -> str:
         return "not possible with wtype"
 
-    def type_characters(self, characters: str, key_delay: int, active_window: str) -> None:
+    def type_characters(self, characters: str, start_delay: float, key_delay: int, active_window: str) -> None:
+        sleep(start_delay)
         args = ["wtype"]
 
         if key_delay > 0:
